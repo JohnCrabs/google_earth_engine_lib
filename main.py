4a2c749909aca54815068ecb805f87eff8e3fbfd
JohnCrabs <kavouras.ioannis@gmail.com>
2021-05-21 14:02:22 +0300

    First Usable Code

--- main.py
+++ main.py
@@ -18,22 +18,57 @@ _LIST_DATE_RANGES_PERIOD = my_cal_v2.break_date_range_to_periods(date_start=_DAT
 _LIST_DATE_RANGES_FOR_PATHS = my_cal_v2.create_string_list_date_range(list_input=_LIST_DATE_RANGES_PERIOD,
                                                                       del_input=my_cal_v2.del_dash,
                                                                       del_output=my_cal_v2.del_none)
+# # Athens
+# point_1 = [23.50, 37.75]
+# point_2 = [24.00, 38.25]
+# pixel_size = 50
+#
+# _EXPORT_NAME = 'S05_ATHENS_CO_'
+# _FOLDER_NAME = 'S05_ATHENS_EUPOLIS'
 
-point_1 = [23.00, 40.50]
-point_2 = [23.25, 40.75]
-pixel_size = 5
+# # Belgrade
+# point_1 = [44.55, 20.20]
+# point_2 = [45.05, 20.70]
+# pixel_size = 50
+#
+# _EXPORT_NAME = 'S05_BELGRADE_CO_'
+# _FOLDER_NAME = 'S05_BELGRADE_EUPOLIS'
+
+# # Gladsaxe
+# point_1 = [55.50, 12.25]
+# point_2 = [56.00, 12.75]
+# pixel_size = 50
+#
+# _EXPORT_NAME = 'S05_GLADSAXE_CO_'
+# _FOLDER_NAME = 'S05_GLADSAXE_EUPOLIS'
+
+# Lodz
+point_1 = [51.50, 19.20]
+point_2 = [52.00, 19.70]
+pixel_size = 50
+
+_EXPORT_NAME = 'S05_LODZ_CO_'
+_FOLDER_NAME = 'S05_LODZ_EUPOLIS'
+
+# # Rome
+# point_1 = [41.65, 12.25]
+# point_2 = [42.15, 12.75]
+# pixel_size = 50
+#
+# _EXPORT_NAME = 'S05_ROME_CO_'
+# _FOLDER_NAME = 'S05_ROME_EUPOLIS'
+
 
 my_df = gee.GoogleEarthEngine()
-dataset_code = gee.GSV_DS_SENTINEL_2_MSI_SR_DKEY
-band_name = [gee.DICT_FULL_DATASET[dataset_code][gee.BANDS_DKEY][gee.RED_DKEY],
-             gee.DICT_FULL_DATASET[dataset_code][gee.BANDS_DKEY][gee.GREEN_DKEY],
-             gee.DICT_FULL_DATASET[dataset_code][gee.BANDS_DKEY][gee.BLUE_DKEY]]
+dataset_code = gee.GSV_DS_SENTINEL_5_OFFL_CO_DKEY
+band_name = [gee.DICT_FULL_DATASET[dataset_code][gee.BANDS_DKEY][gee.S05_CO_COLUMN_NUMBER_DENSITY_DKEY]]
 
 for date_range_index in range(0, len(_LIST_DATE_RANGES_PERIOD)):
     my_df.set_collection_date_range(date_range=_LIST_DATE_RANGES_PERIOD[date_range_index])
     # my_df.set_collection_geometry_from_gee_country_str('Greece')
     my_df.set_collection_geometry_as_a_square_bound(point_1=point_1, point_2=point_2)
     my_df.set_collection(dataset_id=dataset_code)
-    my_df.create_image()
-    export_name = 'S02_EGNATIA_' + str(pixel_size) + '_' + _LIST_DATE_RANGES_FOR_PATHS[date_range_index]
-    my_df.export_to_drive(export_name=export_name, scale_m2_px=pixel_size, folder_name='PANOPTIS_EGNATIA')
+    my_df.create_image_from_band(band_name[0])
+    # my_df.create_image()
+    export_name = _EXPORT_NAME + str(pixel_size) + '_' + _LIST_DATE_RANGES_FOR_PATHS[date_range_index]
+    my_df.export_to_drive(export_name=export_name, scale_m2_px=pixel_size, folder_name=_FOLDER_NAME)
